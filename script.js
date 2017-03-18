28f7c241467bea439f75fdd24ec16daccb78565d
BucketOfMilk <seanbw0518@gmail.com>
2017-03-18 11:51:55 +0000

    done
    It's a personal webpage for a school trip and project.

---
+++ script.js
@@ -0,0 +1,20 @@
+function math(){
+	document.getElementById("text").innerHTML = "I take maths A-Level. It has many parts: C1, C2, C3, C4, M1, D1. It is a 2-year course.";
+	document.getElementById("image").src = "http://tutorial.math.lamar.edu/Classes/CalcII/PowerSeriesandFunctions_files/eq0047P.gif";
+}
+function phys(){
+	document.getElementById("text").innerHTML = "I take physics A-Level. It includes topics such as particle physics, waves, mechanics, materials, electricity, and more.";
+	document.getElementById("image").src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Feynmann_Diagram_Gluon_Radiation.svg/287px-Feynmann_Diagram_Gluon_Radiation.svg.png";
+}
+function comp(){
+	document.getElementById("text").innerHTML = "In computing A-Level, I have been mainly learning Python. And now I'm here!";
+	document.getElementById("image").src = "https://avocaventures.com/wp-content/uploads/2014/11/lines-of-code.jpg";
+}
+function dofe(){
+	document.getElementById("text").innerHTML = "In the Duke of Edinburgh Gold Award, there are 5 sections: volunteering, physical, skill, residential, and expedition.";
+	document.getElementById("image").src = "http://www.gold-expeditions.com/wp-content/themes/gold-expeditions.com/images/headers/slide-01.jpg";
+}
+function epq(){
+	document.getElementById("text").innerHTML = "In my EPQ lessons we work on the extended project as well as study skills and UCAS.";
+	document.getElementById("image").src = "http://www.universitiesuk.ac.uk/news/PublishingImages/ucas-logo.png?RenditionID=7";
+}
